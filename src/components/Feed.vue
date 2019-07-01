f528f68e39c5a9bbc3445f2a1eca4ff5e4d15b1b
vse-monty <49755895+vse-monty@users.noreply.github.com>
2019-07-01 07:52:21 -0500

    babies

--- src/components/Feed.vue
+++ src/components/Feed.vue
@@ -1,27 +1,5 @@
 <template>
   <div class="main-content">
-
-        <v-btn
-        @click="runScript()"
-        color="primary"
-        icon>
-        <v-icon>mdi-arrow-right-bold</v-icon>
-        </v-btn>
-       
-        <v-btn
-        @click="connectTo()"
-        color="primary"
-        icon>
-        <v-icon>mdi-arrow-up-bold</v-icon>
-        </v-btn>
-       
-        <v-btn
-        @click="startServer()"
-        color="primary"
-        icon>
-        <v-icon>mdi-arrow-left-bold</v-icon>
-        </v-btn>
-
     <v-container grid-list-md text-xs-center>
     <v-layout column wrap>
       <v-flex v-for="order in orders" :key="order">
@@ -95,9 +73,9 @@ export default {
   },
   methods: {
     connectTo(){
+      
       console.log('attempting to connect to server')
       this.socketIO.connect();
-      
     },
   }
 }
